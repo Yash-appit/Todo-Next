9b4d8abe6e5270e2bf6cf375c52055b86ff4e276
Yash-appit <yash@appitronsolutions.com>
2026-01-27 15:51:13 +0530

    Second

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,49 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
 import "./globals.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Toaster } from "sonner";
-import { Roboto, Inter } from "next/font/google";
-import localFont from "next/font/local";
-
-const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["400", "500", "700"],
-  variable: "--font-roboto",
-  display: "swap",
-});
-
-const inter = Inter({
-  subsets: ["latin"],
-  variable: "--font-inter",
-  display: "swap",
-});
-
-const satoshi = localFont({
-  src: [
-    { path: "../assets/Fonts/Satoshi-Regular.ttf", weight: "400", style: "normal" },
-    { path: "../assets/Fonts/Satoshi-Medium.ttf", weight: "500", style: "normal" },
-    { path: "../assets/Fonts/Satoshi-Bold.ttf", weight: "700", style: "normal" },
-    { path: "../assets/Fonts/Satoshi-Black.ttf", weight: "900", style: "normal" },
-  ],
-  variable: "--font-satoshi",
-  display: "swap",
-});
-
-const gambetta = localFont({
-  src: "../assets/Fonts/Gambetta-VariableItalic.ttf",
-  variable: "--font-gambetta",
-  display: "swap",
-});
-
-const arial = localFont({
-  src: [
-    { path: "../assets/Fonts/Arial.TTF", weight: "400", style: "normal" },
-    { path: "../assets/Fonts/Arial-bold.TTF", weight: "700", style: "normal" },
-    { path: "../assets/Fonts/Arial-Bold-Italic.TTF", weight: "700", style: "italic" },
-    { path: "../assets/Fonts/Arial-Black.TTF", weight: "900", style: "normal" },
-  ],
-  variable: "--font-arial",
-  display: "swap",
-});
 
 export default function RootLayout({
   children,
@@ -52,7 +9,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${roboto.variable} ${inter.variable} ${satoshi.variable} ${gambetta.variable} ${arial.variable}`} suppressHydrationWarning>
+      <body suppressHydrationWarning>
         <Toaster richColors position="top-center" />
         {children}
       </body>
