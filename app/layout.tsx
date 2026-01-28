d5310d83a2873acc4f2a1993f90304a240370489
Yash-appit <yash@appitronsolutions.com>
2026-01-28 14:05:59 +0530

    Third2

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,20 @@
 import "./globals.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Toaster } from "sonner";
+import { Inter, Roboto } from "next/font/google";
+
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+  display: "swap",
+});
+
+const roboto = Roboto({
+  weight: ["400", "500", "700"],
+  subsets: ["latin"],
+  variable: "--font-roboto",
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -9,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body suppressHydrationWarning>
+      <body suppressHydrationWarning className={`${inter.variable} ${roboto.variable}`}>
         <Toaster richColors position="top-center" />
         {children}
       </body>
